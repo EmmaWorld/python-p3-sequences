66dc910f8e6bd5c9ec83af34fec377be79c6482c
Mang <mangemmanuel07@gmail.com>
2023-05-18 21:33:22 +0800

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        sequence = [0, 1]
+        while len(sequence) < length:
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+        print(sequence)
+
